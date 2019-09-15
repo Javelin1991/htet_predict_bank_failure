3734ce52329fef3b85a9bf0a8cd4a1bee9a58438
Htet <htet.naing91@gmail.com>
2019-09-15 18:19:06 +0800

    Calculate EER, TYPE I, TYPE II ERROR

--- main/htet_test_bank_classification_safin_frie.m
+++ main/htet_test_bank_classification_safin_frie.m
@@ -1,13 +1,8 @@
-% XXXXXXXXXXXXXXXXXXXXXXXXXXXX htet_test_bank_classification_emfis XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+% XXXXXXXXXXXXXXXXXXXXXXXXXXXX htet_test_bank_classification_safin_frie XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 %
 % Author    :   Htet
 % Date      :   Sep 11, 2019
-% Function  :   used to test bank failure prediction/classification using eMFIS(FRIE)
-% CV refers to cross validation and 5-fold CV is used below for:
-% last year (t) prediction
-% one year prior (t-1) prediction
-% two year prior (t-2) prediction
-% It takes around 70 min to run this file
+% Function  :   used to test bank failure prediction/classification using SaFIN(FRIE)
 % XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 clear;
@@ -36,14 +31,22 @@ Epochs = 300;
 unclassified_count = 0;
 eer_count = 0;
 after_threshold = zeros(length(testData),1);
-cut_off = 0.2550;
+
+% adjust cut_off value
+cut_off = 0.185;
+
 Acc = [];
 Acc2 = [];
 FPR = [];
 FNR = [];
 Optimal_Ratio = [];
 Min_Weighted_Sum = [];
-CURR_SMALLEST = 100;
+CURR_SMALLEST = intmax;
+fpr_penalized_cost = 1;
+fnr_penalized_cost = [1; 5; 10; 15; 20; 25; 30];
+MIN_EER = [];
+MIN_FPR = [];
+MIN_FNR = [];
 
 %%%%%%%%%%%%%% after finding the best EER value, calculate misclassification cost %%%%%%%%%%%%%%%%%
 
@@ -69,82 +72,93 @@ net_result.EER = (eer_count * 100)/test_examples;
 
 [TP, FP, TN, FN, fnr, fpr, acc, min_weight_sum, optimal_ratio] = htet_get_classification_results(testData(:,4), after_threshold(:,1))
 
-i = 1;
-counter = 1;
-FNR_Cost = 1;
-FPR_Cost = 1;
-
-while (i <= 30)
-  FNR_Cost = FNR_Cost * i * fnr;
-  FPR_Cost = FPR_Cost * fpr;
-  FPR = [FPR; FPR_Cost];
-  FNR = [FNR; FNR_Cost];
-  i = 5 * counter;
-  counter = counter + 1;
-  FNR_Cost = 1;
-  FPR_Cost = 1;
-end
-
 
 %%%%%%%%%%%%%% to find out the best cut-off point or EER value %%%%%%%%%%%%%%%%%
 %
-% while(cut_off <= 1)
-%   for i=1: length(testData)
-%       if net_out(i) > cut_off
-%           after_threshold(i) = 1;
-%       elseif net_out(i) < cut_off
-%           after_threshold(i) = 0;
-%       elseif net_out(i) == cut_off
-%           after_threshold(i) = cut_off;
-%           eer_count = eer_count + 1;
-%       else
-%           unclassified_count = unclassified_count + 1;
-%       end
+% for z=1:length(fnr_penalized_cost)
+%
+%   while(cut_off <= 1)
+%     for i=1: length(testData)
+%         if net_out(i) > cut_off
+%             after_threshold(i) = 1;
+%         elseif net_out(i) < cut_off
+%             after_threshold(i) = 0;
+%         elseif net_out(i) == cut_off
+%             after_threshold(i) = cut_off;
+%             eer_count = eer_count + 1;
+%         else
+%             unclassified_count = unclassified_count + 1;
+%         end
+%     end
+%     net_result.predicted = {net_out};
+%     net_result.after_threshold = {after_threshold};
+%     correct_predictions = length(find(testData(:,4) - after_threshold(:,1) == 0));
+%     test_examples = length(testData);
+%     net_result.accuracy = (correct_predictions * 100)/test_examples;
+%     net_result.unclassified = (unclassified_count * 100)/test_examples;
+%     net_result.EER = (eer_count * 100)/test_examples;
+%
+%     [TP, FP, TN, FN, fnr, fpr, acc, min_weight_sum, optimal_ratio] = htet_get_classification_results(testData(:,4), after_threshold(:,1))
+%
+%     FPR = [FPR; fpr];
+%     FNR = [FNR; fnr];
+%
+%     fpr_cost = fpr * fpr_penalized_cost;
+%     fnr_cost = fnr * fnr_penalized_cost(z,:);
+%
+%     TOTAL = (fpr_cost + fnr_cost)/2;
+%
+%     if TOTAL < CURR_SMALLEST
+%       min_eer_point = cut_off;
+%       CURR_SMALLEST = TOTAL;
+%       BEST_FPR = fpr;
+%       BEST_FNR = fnr;
+%     end
+%
+%     Acc = [Acc; net_result.accuracy];
+%     Acc2 = [Acc2; acc];
+%     Optimal_Ratio = [Optimal_Ratio; optimal_ratio];
+%     Min_Weighted_Sum = [Min_Weighted_Sum; min_weight_sum];
+%
+%     cut_off = cut_off + 0.001;
+%     unclassified_count = 0;
+%     eer_count = 0;
+%     after_threshold = zeros(length(testData),1);
 %   end
-%   net_result.predicted = {net_out};
-%   net_result.after_threshold = {after_threshold};
-%   correct_predictions = length(find(testData(:,4) - after_threshold(:,1) == 0));
-%   test_examples = length(testData);
-%   net_result.accuracy = (correct_predictions * 100)/test_examples;
-%   net_result.unclassified = (unclassified_count * 100)/test_examples;
-%   net_result.EER = (eer_count * 100)/test_examples;
 %
-%   [TP, FP, TN, FN, fnr, fpr, acc, min_weight_sum, optimal_ratio] = htet_get_classification_results(testData(:,4), after_threshold(:,1))
+%   MIN_EER = [MIN_EER; min_eer_point];
+%   MIN_FPR = [MIN_FPR; BEST_FPR];
+%   MIN_FNR = [MIN_FNR; BEST_FNR];
 %
-%   FPR = [FPR; fpr];
-%   FNR = [FNR; fnr];
-%
-%   TOTAL = (fpr + fnr)/2;
-%
-%   if TOTAL < CURR_SMALLEST
-%     min_eer_point = cut_off;
-%     CURR_SMALLEST = TOTAL;
-%   end
-%
-%   Acc = [Acc; net_result.accuracy];
-%   Acc2 = [Acc2; acc];
-%   Optimal_Ratio = [Optimal_Ratio; optimal_ratio];
-%   Min_Weighted_Sum = [Min_Weighted_Sum; min_weight_sum];
-%
-%   cut_off = cut_off + 0.001;
+%   % Labels = ["0.05", "0.1", "0.1"', "0.2", "0.25", "0.3", "0.35", "0.4", "0.45", "0.5", "0.55", "0.6", "0.65", "0.7", "0.75", "0.8", "0.85", "0.9", "0.95", "1"];
+%   % reset all values
 %   unclassified_count = 0;
 %   eer_count = 0;
 %   after_threshold = zeros(length(testData),1);
+%   cut_off = 0;
+%   Acc = [];
+%   Acc2 = [];
+%   FPR = [];
+%   FNR = [];
+%   Optimal_Ratio = [];
+%   Min_Weighted_Sum = [];
+%   CURR_SMALLEST = intmax;
+%   fpr_penalized_cost = 1;
+%   TOTAL = 0;
+  %
+  % figure;
+  % bar(Acc); % plot the matrix
+  % % set(gca, 'XTick', 1:20); % center x-axis ticks on bins
+  % % set(gca, 'XTickLabel', Labels); % set x-axis labels
+  % title('Bank Failure Classification Accuracy', 'FontSize', 14); % set title
+  % colormap('jet'); % set the colorscheme
+  %
+  % figure;
+  % plot(FNR, FPR); % plot the matrix
+  % title('Bank Failure Classification Accuracy', 'FontSize', 14); % set title
+  % colormap('jet'); % set the colorscheme
+
 % end
-%
-% % Labels = ["0.05", "0.1", "0.1"', "0.2", "0.25", "0.3", "0.35", "0.4", "0.45", "0.5", "0.55", "0.6", "0.65", "0.7", "0.75", "0.8", "0.85", "0.9", "0.95", "1"];
-%
-% figure;
-% bar(Acc); % plot the matrix
-% % set(gca, 'XTick', 1:20); % center x-axis ticks on bins
-% % set(gca, 'XTickLabel', Labels); % set x-axis labels
-% title('Bank Failure Classification Accuracy', 'FontSize', 14); % set title
-% colormap('jet'); % set the colorscheme
-%
-% figure;
-% plot(FNR, FPR); % plot the matrix
-% title('Bank Failure Classification Accuracy', 'FontSize', 14); % set title
-% colormap('jet'); % set the colorscheme
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
