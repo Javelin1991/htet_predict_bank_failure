28ee711d85241611aca8fa90bd6b16d5c54d8e9b
Htet Naing <htet.naing91@gmail.com>
2019-09-19 16:19:50 +0800

    Add code to generate DET plots

---
+++ main/htet_generate_det_plot_increased_data.m
@@ -0,0 +1,67 @@
+% XXXXXXXXXXXXXXXXXXXXXXXXXXXX htet_playground_test_any_code_snippet XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+%
+% Author    :   Htet
+% Date      :   Sep 11, 2019
+% Function  :   htet_generate_det_plot
+% XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+clc;
+clear;
+
+load safin_cv5_top3_feat_results;
+color_str = 'rgbcm'
+MEAN_INCREASED = [];
+ACC = [];
+mean_eer_increased_data = 0;
+
+for j=1:3
+    figure;
+    bisector = net_result_for_last_record(1).output.bisector;
+    plot(bisector, bisector, 'k'); % plot the matrix
+    hold on;
+    mean_eer_increased_data = 0;
+    for i=1:5
+        switch j
+            case 1
+                % the last available record
+                fpr = net_result_for_last_record(i).output.all_fpr{1, 1}
+                fnr = net_result_for_last_record(i).output.all_fnr{1, 1}
+                mean_eer_increased_data = mean_eer_increased_data + net_result_for_last_record(i).output.MIN_EER(1,1);
+                plot(fnr, fpr, color_str(i)); % plot the matrix
+                title('Bank Failure Detection Error Tradeoff (last avaiable)', 'FontSize', 14); % set title
+                colormap('jet'); % set the colorscheme
+            case 2
+                % one year priror
+                fpr = net_result_for_one_year_prior(i).output.all_fpr{1, 1}
+                fnr = net_result_for_one_year_prior(i).output.all_fnr{1, 1}
+                mean_eer_increased_data = mean_eer_increased_data + net_result_for_one_year_prior(i).output.MIN_EER(1,1);
+                plot(fnr, fpr, color_str(i)); % plot the matrix
+                title('Bank Failure Detection Error Tradeoff (one year prior)', 'FontSize', 14); % set title
+                colormap('jet'); % set the colorscheme
+            case 3
+                % two year priror
+                fpr = net_result_for_two_year_prior(i).output.all_fpr{1, 1}
+                fnr = net_result_for_two_year_prior(i).output.all_fnr{1, 1}
+                mean_eer_increased_data = mean_eer_increased_data + net_result_for_two_year_prior(i).output.MIN_EER(1,1);
+                plot(fnr, fpr, color_str(i)); % plot the matrix
+                title('Bank Failure Detection Error Tradeoff (two year prior)', 'FontSize', 14); % set title
+                colormap('jet'); % set the colorscheme
+        end
+
+        % '-'
+        % Solid line (default)
+        %
+        % '--'
+        % Dashed line
+        %
+        % ':'
+        % Dotted line
+        %
+        % '-.'
+    end
+    legend('EER','CV1','CV2', 'CV3', 'CV4', 'CV5');
+    hold off;
+    mean_eer_increased_data = mean_eer_increased_data/5;
+    MEAN_INCREASED = [MEAN_INCREASED; mean_eer_increased_data];
+    acc = 100 - mean_eer_increased_data
+    ACC = [ACC; acc];
+end
--- main/htet_test_bank_classification_safin_frie.m
+++ main/htet_test_bank_classification_safin_frie.m
@@ -10,9 +10,13 @@ clear;
 clc;
 
 % load '5_Fold_CVs_with_top_3_features';
-load CV1_Classification;
-load CV2_Classification;
-load CV3_Classification;
+% load CV1_Classification;
+% load CV2_Classification;
+% load CV3_Classification;
+load 'Reconstructed_Data_LL';
+
+FB = RECONSTRUCTED_DATA{1, 1};
+SB = RECONSTRUCTED_DATA{2, 1};
 
 IND = 3;
 OUTD = 1;
@@ -22,49 +26,49 @@ Eta = 0.01;
 Forgetfactor = 0.99;
 Epochs = 0;
 
-% for k = 1:3
-%   backward_offset = k-1;
-%   Failed_Banks_Group_By_Bank_ID = [];
-%   Survived_Banks_Group_By_Bank_ID = [];
-%
-%   output_1 = htet_filter_bank_data_by_index(SB, backward_offset);
-%   output_2 = htet_filter_bank_data_by_index(FB, backward_offset);
-%
-%   Survived_Banks_Group_By_Bank_ID = output_1.result;
-%   Failed_Banks_Group_By_Bank_ID = output_2.result;
-%
-%   if k == 1
-%     CV1_with_top_3_features = htet_generate_cross_validation_data(Survived_Banks_Group_By_Bank_ID, Failed_Banks_Group_By_Bank_ID, 5, true);
-%   elseif k == 2
-%     CV2_with_top_3_features = htet_generate_cross_validation_data(Survived_Banks_Group_By_Bank_ID, Failed_Banks_Group_By_Bank_ID, 5, true);
-%   else
-%     CV3_with_top_3_features = htet_generate_cross_validation_data(Survived_Banks_Group_By_Bank_ID, Failed_Banks_Group_By_Bank_ID, 5, true);
-%   end
-% end
+for k = 1:3
+  backward_offset = k-1;
+  Failed_Banks_Group_By_Bank_ID = [];
+  Survived_Banks_Group_By_Bank_ID = [];
+
+  output_1 = htet_filter_bank_data_by_index(SB, backward_offset);
+  output_2 = htet_filter_bank_data_by_index(FB, backward_offset);
+
+  Survived_Banks_Group_By_Bank_ID = output_1.result;
+  Failed_Banks_Group_By_Bank_ID = output_2.result;
+
+  if k == 1
+    CV1_with_top_3_features = htet_generate_cross_validation_data(Survived_Banks_Group_By_Bank_ID, Failed_Banks_Group_By_Bank_ID, 5, true);
+  elseif k == 2
+    CV2_with_top_3_features = htet_generate_cross_validation_data(Survived_Banks_Group_By_Bank_ID, Failed_Banks_Group_By_Bank_ID, 5, true);
+  else
+    CV3_with_top_3_features = htet_generate_cross_validation_data(Survived_Banks_Group_By_Bank_ID, Failed_Banks_Group_By_Bank_ID, 5, true);
+  end
+end
 
 threshold = 0;
 
-for cv_num = 1:1
+for cv_num = 1:5
   disp('');
   formatSpec = 'The current cv used is: %d';
   str = sprintf(formatSpec,cv_num)
   disp(str);
 
-  D0 = CV1{cv_num};
-  D1 = CV2{cv_num};
-  D2 = CV3{cv_num};
-
-  D0 = D0(1:30,[3 7 10 2]);
-  D1 = D1(:,[3 7 10 2]);
-  D2 = D2(:,[3 7 10 2]);
-
-  % D0 = CV1_with_top_3_features{cv_num};
-  % D1 = CV2_with_top_3_features{cv_num};
-  % D2 = CV3_with_top_3_features{cv_num};
+  % D0 = CV1{cv_num};
+  % D1 = CV2{cv_num};
+  % D2 = CV3{cv_num};
   %
-  % D0 = D0(:,[3 4 5 2]);
-  % D1 = D1(:,[3 4 5 2]);
-  % D2 = D2(:,[3 4 5 2]);
+  % D0 = D0(1:30,[3 7 10 2]);
+  % D1 = D1(:,[3 7 10 2]);
+  % D2 = D2(:,[3 7 10 2]);
+
+  D0 = CV1_with_top_3_features{cv_num};
+  D1 = CV2_with_top_3_features{cv_num};
+  D2 = CV3_with_top_3_features{cv_num};
+
+  D0 = D0(:,[3 4 5 2]);
+  D1 = D1(:,[3 4 5 2]);
+  D2 = D2(:,[3 4 5 2]);
 
   start_test = (size(D0, 1) * 0.2) + 1;
   trainData_D0 = D0(1:start_test-1,:);
@@ -84,23 +88,19 @@ for cv_num = 1:1
   result_0.output = output_0;
   net_result_for_last_record(cv_num) = result_0;
 
-%   [net_out_1, net_structure_1] = Run_SaFIN(trainData_D1,testData_D1,IND,OUTD,Alpha,Beta,Epochs,Eta,Forgetfactor);
-%   output_1 = htet_find_optimal_cut_off(testData_D1(:,4), net_out_1, threshold);
-%   result_1.net_out = net_out_1;
-%   result_1.net_structure = net_structure_1;
-%   result_1.output = output_1;
-%   net_result_for_one_year_prior(cv_num) = result_1;
-%
-%   [net_out_2, net_structure_2] = Run_SaFIN(trainData_D2,testData_D2,IND,OUTD,Alpha,Beta,Epochs,Eta,Forgetfactor);
-%   output_2 = htet_find_optimal_cut_off(testData_D2(:,4), net_out_2, threshold);
-%   result_2.net_out = net_out_2;
-%   result_2.net_structure = net_structure_2;
-%   result_2.output = output_2;
-%   net_result_for_two_year_prior(cv_num) = result_2;
+  [net_out_1, net_structure_1] = Run_SaFIN(trainData_D1,testData_D1,IND,OUTD,Alpha,Beta,Epochs,Eta,Forgetfactor);
+  output_1 = htet_find_optimal_cut_off(testData_D1(:,4), net_out_1, threshold);
+  result_1.net_out = net_out_1;
+  result_1.net_structure = net_structure_1;
+  result_1.output = output_1;
+  net_result_for_one_year_prior(cv_num) = result_1;
+
+  [net_out_2, net_structure_2] = Run_SaFIN(trainData_D2,testData_D2,IND,OUTD,Alpha,Beta,Epochs,Eta,Forgetfactor);
+  output_2 = htet_find_optimal_cut_off(testData_D2(:,4), net_out_2, threshold);
+  result_2.net_out = net_out_2;
+  result_2.net_structure = net_structure_2;
+  result_2.output = output_2;
+  net_result_for_two_year_prior(cv_num) = result_2;
 
   disp('Processing of one CV group has completed');
 end
-
-
-% load handel
-% sound(y,Fs)
