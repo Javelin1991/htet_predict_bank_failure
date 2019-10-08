ea4ef0d3b0d2879378ab061a14c6e6369732dcbd
Htet Naing <htet.naing91@gmail.com>
2019-10-08 14:19:38 +0800

    Automate the accuracy threshold setting process

--- main/htet_test_bank_classification_safin_frie_with_feat_select.m
+++ main/htet_test_bank_classification_safin_frie_with_feat_select.m
@@ -39,7 +39,7 @@ Omega = 0.7;
 Gamma = 0.1;
 forget = 1;
 tau = 0.57;
-accuracy_threshold = 80;
+decrement_step = 10;
 
 threshold = 0;
 best_mean_acc = 0;
@@ -73,6 +73,7 @@ for cv_num = 1:5
   % Data = CV1{cv_num,1}; % last available
   % Data = CV2{cv_num,1}; % one year prior
   % Data = CV3{cv_num,1}; % two year prior
+
   % Data = Data(:,[3 7 10 2]); % 3 covariates
 
   % for 9 variable, timeline - last available
@@ -92,116 +93,130 @@ for cv_num = 1:5
 
   % combined last 3 timeline prediction
   % Data = CV_3T{cv_num,1};
+  accuracy_threshold = 100;
 
-  % top 3 features with increased data set
-  Data = CV{cv_num,1};
-  Data = Data(:,[3:5 2])
+  while (accuracy_threshold > 0) {
 
-  epoch = 0; % this epoch is just for a predefined number of iterations
-  best_acc = 0;
-  best_eer = 100;
-  D0 = Data;
-  limit = size(D0,2) - 1;
 
-  %%% using feature selection algorithm to rank features %%%
-  [idx,scores] = fscmrmr(D0(:,[1:limit]), D0(:,limit+1));
-  input = D0(:,[1:limit]);
-  input = input(:,idx);
-  Labels = Labels(:,idx);
-  output = D0(:,limit+1);
-  D0 = [input output];
+      % top 3 features with increased data set
+      Data = CV{cv_num,1};
+      Data = Data(:,[3:5 2])
 
-  %%% get train data and validation data %%%
-  start_test = (size(Data, 1) * 0.2) + 1;
-  trainData_D0 = D0(1:start_test-1,:);
-  start_validate = floor((size(trainData_D0, 1) * 0.8) + 1);
-  valData_D0 = trainData_D0(start_validate:length(trainData_D0),:);
-  trainData_D0 = trainData_D0(1:start_validate-1,:); % reduce train data size to 80%
+      epoch = 0; % this epoch is just for a predefined number of iterations
+      best_acc = 0;
+      best_eer = 100;
+      D0 = Data;
+      limit = size(D0,2) - 1;
 
-  best_list = [];
-  best_val_list = [];
-  best_indices = [];
-  best_labels = [];
-  filter_indices = [];
+      %%% using feature selection algorithm to rank features %%%
+      [idx,scores] = fscmrmr(D0(:,[1:limit]), D0(:,limit+1));
+      input = D0(:,[1:limit]);
+      input = input(:,idx);
+      Labels = Labels(:,idx);
+      output = D0(:,limit+1);
+      D0 = [input output];
 
-  %%% for input dimension 1 to limit %%%
-  for l=1:limit
+      %%% get train data and validation data %%%
+      start_test = (size(Data, 1) * 0.2) + 1;
+      trainData_D0 = D0(1:start_test-1,:);
+      start_validate = floor((size(trainData_D0, 1) * 0.8) + 1);
+      valData_D0 = trainData_D0(start_validate:length(trainData_D0),:);
+      trainData_D0 = trainData_D0(1:start_validate-1,:); % reduce train data size to 80%
 
-    %%% maintain a list for current iteration %%%
-    curr_list = [best_list trainData_D0(:,l) trainData_D0(:,limit+1)]
-    curr_valData = [best_val_list valData_D0(:,l) valData_D0(:,limit+1)]
-    target = size(curr_list,2);
+      best_list = [];
+      best_val_list = [];
+      best_indices = [];
+      best_labels = [];
+      filter_indices = [];
 
-    %%% input dimension and output dimension %%%
-    IND = size(curr_list,2) - 1;
-    OUTD = 1;
+      %%% for input dimension 1 to limit %%%
+      for l=1:limit
 
-    % ensemble learning with hcl
-    [net_out, system] = Run_SaFIN_FRIE(1,curr_list,curr_valData,IND,OUTD,Epochs,Eta,Sigma0,Forgetfactor, forget,Lamda, tau,Rate, Omega, Gamma);
-    output = htet_find_optimal_cut_off(curr_valData(:,IND+OUTD), net_out, 0);
-    curr_eer = output.MIN_EER(1,1);
-    curr_acc = 100 - output.MIN_EER(1,1);
+        %%% maintain a list for current iteration %%%
+        curr_list = [best_list trainData_D0(:,l) trainData_D0(:,limit+1)]
+        curr_valData = [best_val_list valData_D0(:,l) valData_D0(:,limit+1)]
+        target = size(curr_list,2);
 
-    %%% if the current error is lower than best error, then update the best error %%%
-    if curr_acc > accuracy_threshold && curr_acc > best_acc
-        best_list = [best_list trainData_D0(:,l)];
-        best_val_list = [best_val_list valData_D0(:,l)];
+        %%% input dimension and output dimension %%%
+        IND = size(curr_list,2) - 1;
+        OUTD = 1;
 
-        best_indices = [best_indices, idx(1,l)];
-        best_labels = [best_labels, Labels(1,l)];
-        filter_indices = [filter_indices, l];
+        % ensemble learning with hcl
+        [net_out, system] = Run_SaFIN_FRIE(1,curr_list,curr_valData,IND,OUTD,Epochs,Eta,Sigma0,Forgetfactor, forget,Lamda, tau,Rate, Omega, Gamma);
+        output = htet_find_optimal_cut_off(curr_valData(:,IND+OUTD), net_out, 0);
+        curr_eer = output.MIN_EER(1,1);
+        curr_acc = 100 - output.MIN_EER(1,1);
 
-        best_features = best_indices;
-        best_eer = curr_eer;
-        best_acc = curr_acc;
-    end
+        %%% if the current error is lower than best error, then update the best error %%%
+        if curr_acc > accuracy_threshold && curr_acc > best_acc
+            best_list = [best_list trainData_D0(:,l)];
+            best_val_list = [best_val_list valData_D0(:,l)];
+
+            best_indices = [best_indices, idx(1,l)];
+            best_labels = [best_labels, Labels(1,l)];
+            filter_indices = [filter_indices, l];
+
+            best_features = best_indices;
+            best_eer = curr_eer;
+            best_acc = curr_acc;
+        end
+      end
+
+      have_best_feature_list = true;
+
+      if size(best_indices,1) == 0
+        disp('The required accuracy cannot be found! Reducing the step...');
+        accuracy_threshold = accuracy_threshold - decrement_step;
+        have_best_feature_list = false;
+        continue;
+      end
+
+
+
+      %%% getting the label dimension %%%
+      last_idx = size(Data,2);
+
+      %%% preparing the data for new training + testing %%%
+      %%% reuse validation data now for training as well %%%
+      Data = Data(:,[best_features last_idx]);
+      start_test = (size(Data, 1) * 0.2) + 1;
+      train = Data(1:start_test-1,:);
+      test = Data(start_test:length(Data), :);
+      target = size(train,2);
+
+      limit = target-1;
+      IND = limit;
+      OUTD = 1;
+      [net_out, system] = Run_SaFIN_FRIE(1,train,test,IND,OUTD,Epochs,Eta,Sigma0,Forgetfactor, forget,Lamda, tau,Rate, Omega, Gamma);
+      output = htet_find_optimal_cut_off(test(:,IND+OUTD), net_out, 0);
+
+      % summary includes 3 columns
+      % column 1 is raw output value,
+      % column 2 is the value converted from raw output to either 0 or 1
+      % column 3 is the ground truth label
+      result.summary = [net_out cell2mat(output.BEST_AFTER_THRESHOLD(1,1)) test(:,IND+OUTD)];
+      result.net_structure = system;
+      result.output = output;
+      result.MIN_CUT_OFF = output.MIN_CUT_OFF(1,1);
+      result.FNR = output.MIN_FNR(1,1);
+      result.FPR = output.MIN_FPR(1,1);
+      result.EER = output.MIN_EER(1,1);
+      result.ACC = 100 - output.MIN_EER(1,1);
+      result.Feat = IND;
+      result.Rules = system.ruleCount;
+      result.best_feat = best_indices;
+      result.best_labels = best_labels;
+
+      %%% store the system for each CV group %%%
+      BEST_SYSTEMS = [BEST_SYSTEMS; result]
+
+      %%% calculate mean accuracy %%%
+      best_mean_acc = best_mean_acc + result.ACC;
+
+      if (have_best_feature_list)
+          break;
+      end
   end
-
-  if size(best_indices,1) == 0
-    disp('The required accuracy cannot be found! Please adjust the accuracy_threshold setting');
-    return;
-  end
-
-
-  %%% getting the label dimension %%%
-  last_idx = size(Data,2);
-
-  %%% preparing the data for new training + testing %%%
-  %%% reuse validation data now for training as well %%%
-  Data = Data(:,[best_features last_idx]);
-  start_test = (size(Data, 1) * 0.2) + 1;
-  train = Data(1:start_test-1,:);
-  test = Data(start_test:length(Data), :);
-  target = size(train,2);
-
-  limit = target-1;
-  IND = limit;
-  OUTD = 1;
-  [net_out, system] = Run_SaFIN_FRIE(1,train,test,IND,OUTD,Epochs,Eta,Sigma0,Forgetfactor, forget,Lamda, tau,Rate, Omega, Gamma);
-  output = htet_find_optimal_cut_off(test(:,IND+OUTD), net_out, 0);
-
-  % summary includes 3 columns
-  % column 1 is raw output value,
-  % column 2 is the value converted from raw output to either 0 or 1
-  % column 3 is the ground truth label
-  result.summary = [net_out cell2mat(output.BEST_AFTER_THRESHOLD(1,1)) test(:,IND+OUTD)];
-  result.net_structure = system;
-  result.output = output;
-  result.MIN_CUT_OFF = output.MIN_CUT_OFF(1,1);
-  result.FNR = output.MIN_FNR(1,1);
-  result.FPR = output.MIN_FPR(1,1);
-  result.EER = output.MIN_EER(1,1);
-  result.ACC = 100 - output.MIN_EER(1,1);
-  result.Feat = IND;
-  result.Rules = system.ruleCount;
-  result.best_feat = best_indices;
-  result.best_labels = best_labels;
-
-  %%% store the system for each CV group %%%
-  BEST_SYSTEMS = [BEST_SYSTEMS; result]
-
-  %%% calculate mean accuracy %%%
-  best_mean_acc = best_mean_acc + result.ACC;
 end
 
 best_mean_acc = best_mean_acc/5;
