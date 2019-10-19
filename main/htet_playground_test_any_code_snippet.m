4a427bd188dadf57b3d156368d77dedf1f2df7e3
Htet <htet.naing91@gmail.com>
2019-10-19 10:54:14 +0800

    Save the last working state

--- main/htet_playground_test_any_code_snippet.m
+++ main/htet_playground_test_any_code_snippet.m
@@ -11,5 +11,38 @@ clear;
 clc;
 close all;
 
-load Pre_trained_Systems_Lateral_Prediction;
-load Pre_trained_systems_Longitudinal_Prediction;
+load CV1_Classification;
+
+tr_fb_cv1 = [];
+tt_fb_cv1 = [];
+
+tr_sb_cv1 = [];
+tt_sb_cv1 = [];
+
+D = CV1{1,1}
+
+start_test = size(D,1) * 0.2 + 1;
+
+D_tr = D(1:start_test-1, :)
+D_tt = D(start_test:size(D,1),:)
+
+for i=1:size(D_tr,1)
+    if D_tr(i,2) == 1
+        tr_fb_cv1 = [tr_fb_cv1; D_tr(i,:)]
+    else
+        tr_sb_cv1 = [tr_sb_cv1; D_tr(i,:)]
+    end
+end
+
+for i=1:size(D_tt,1)
+    if D_tt(i,2) == 1
+        tt_fb_cv1 = [tt_fb_cv1; D_tt(i,:)]
+    else
+        tt_sb_cv1 = [tt_sb_cv1; D_tt(i,:)]
+    end
+end
+
+TRAIN_DATA = (vertcat(tr_fb_cv1, tr_sb_cv1))'
+TRAIN_DATA([1 2 6 13], :) = []
+TEST_DATA = (vertcat(tt_fb_cv1, tt_sb_cv1))'
+TEST_DATA([1 2 6 13], :) = []
